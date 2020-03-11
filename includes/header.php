5e0336cea4ae1fad4f36b93c1592b913ce0ee652
Monzerath Soriano <53834045+monzerath@users.noreply.github.com>
2020-03-11 09:11:54 -0500

    php

---
+++ includes/header.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <title>Petalz</title>
+    </head>
+  <body>
+
+ <!--Navigation-->
+<nav class="navbar navbar-expand-lg navbar-light sticky-top">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="nav navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home <span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="menu.php">Menu</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">2</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">3</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<!--End of Navigation-->
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include 'includes/header.php';?>
+    
+<!--Start of Carousel-->
+<div id="carousel" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../Petalz//Pictures/welcome.jpg" class="d-block w-100" alt="...">
+      <div class="caption text-center outline-dark ">
+        <h1>Welcome to Petalz</h1>
+        <a class="btn btn-outline-dark btn-lg" href="#">Get Started</a>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<!--End of Carousel-->
+  </body>
+</html>
